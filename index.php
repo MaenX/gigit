22aca7883134a38653f61a5f3bd2a56875ee0524
MaenX <30519556+MaenX@users.noreply.github.com>
2019-08-24 10:16:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+
+	<title>oalah</title>
+</head>
+<body>
+	<header class="cd-main-header">
+		<a class="cd-logo" href="#"><img src="oalah.svg" alt="Logo" width="72px" ></a>
+
+		<ul class="cd-header-buttons">
+			<li><a class="cd-search-trigger" href="#cd-search">Search<span></span></a></li>
+			<li><a class="cd-nav-trigger" href="#cd-primary-nav">Menu<span></span></a></li>
+		</ul> <!-- cd-header-buttons -->
+	</header>
+
+	<main class="cd-main-content">
+		<!-- your content here -->
+aa
+		<div class="cd-overlay"></div>
+	</main>
+
+	<nav class="cd-nav">
+		<ul id="cd-primary-nav" class="cd-primary-nav is-fixed">
+			<li class="has-children">
+				<a href="#">Menu List</a>
+
+				<ul class="cd-secondary-nav is-hidden">
+					<li class="go-back"><a href="#">Back</a></li>
+					<li class="see-all"></li>
+
+					<li class="has-children">
+						<a href="#">Genres</a>
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#">Back</a></li>
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Adventure</a></li>
+							<li><a href="#">Comedy</a></li>
+							<li><a href="#">Drama</a></li>
+							<li><a href="#">Fantasy</a></li>
+							<li><a href="#">Harem</a></li>
+							<li><a href="#">Horror</a></li>
+							<li><a href="#">Magic</a></li>
+							<li><a href="#">Mecha</a></li>
+							<li><a href="#">Music</a></li>
+							<li><a href="#">Mystery</a></li>
+							<li><a href="#">Parody</a></li>
+							<li><a href="#">Romance</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Release Year</a>
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#">Back</a></li>
+							<li><a href="#">2019</a></li>
+							<li><a href="#">2018</a></li>
+							<li><a href="#">2017</a></li>
+							<li><a href="#">2016</a></li>
+							<li><a href="#">2015</a></li>
+							<li><a href="#">2014</a></li>
+							<li><a href="#">2013</a></li>
+							<li><a href="#">2012</a></li>
+							<li><a href="#">2011</a></li>
+							<li><a href="#">2010</a></li>
+							<li><a href="#">2009</a></li>
+							<li><a href="#">2008</a></li>
+							<li><a href="#">2007</a></li>
+							<li><a href="#">2006</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Studios</a>
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#">Back</a></li>
+							<li><a href="#">Bandai Namco Pictures</a></li>
+							<li><a href="#">Kyoto Animation</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Status</a>
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#">Back</a></li>
+							<li><a href="#">Currently Airing</a></li>
+							<li><a href="#">Finished Airing</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+
+			<li><a href="#">Anime List</a></li>
+		</ul> <!-- primary-nav -->
+	</nav> <!-- cd-nav -->
+
+	<div id="cd-search" class="cd-search">
+		<form>
+			<input type="search" placeholder="Search..">
+		</form>
+	</div>
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/jquery.mobile.custom.min.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
